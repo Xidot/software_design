6dd6957a185c17b9bd2cbb622756d1304b9b7840
Sofia <sophia.konovalova2@gmail.com>
2020-02-04 17:17:32 +0100

    test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("please work for the love of god i beg of you!");
     }
 
 }
