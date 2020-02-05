16b4e10a6686cfe396670554605eb254943bfedd
Sofia <sophia.konovalova2@gmail.com>
2020-02-05 13:31:38 +0100

    added example game window, new package for any graphics rendering - Render. Run the Main class to see it :)

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,7 +4,8 @@ import render.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("please work for the love of god i beg of you!");
+    	Render render = new Render();
+    	System.out.println("please work for the love of god i beg of you!");
     }
 
 }
--- src/main/java/render/Render.java
+++ src/main/java/render/Render.java
@@ -1,4 +1,36 @@
 package render;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Container;
+
 public class Render {
+	JFrame window;
+	Container con;
+	JPanel titlePanel;
+	JLabel titleLabel;
+	Font titleFont = new Font("Times New Roman", Font.PLAIN, 40);
+
+	public Render() {
+		window = new JFrame();
+		window.setSize(800, 600);
+		window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.black);
+		window.setLayout(null);
+		window.setVisible(true);
+		con = window.getContentPane();
+
+		titlePanel = new JPanel();
+		titlePanel.setBounds(100, 100, 600, 150);
+		titlePanel.setBackground(Color.black);
+		con.add(titlePanel);
+
+		titleLabel = new JLabel("yeet boi");
+		titlePanel.setForeground(Color.white);
+		titleLabel.setFont(titleFont);
+		titlePanel.add(titleLabel);
+	}
 }
